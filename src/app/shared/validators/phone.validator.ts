29642aa6a562f807b9da0041f82674763e2aa4fd
bednayb <bednayb@gmail.com>
2019-10-24 22:34:16 +0200

    fix phone validator

--- src/app/shared/validators/phone.validator.ts
+++ src/app/shared/validators/phone.validator.ts
@@ -5,8 +5,8 @@ export class PhoneValidador {
 
   static validate(control: AbstractControl): { [key: string]: any } | null {
 
-    const minLength = 10;
-    const maxLength = 11;
+    const minLength = 11;
+    const maxLength = 12;
 
     // + and 10 or 11 digit exp: +12345123451
     const regexp1 = /^[\+]+[0-9]/;
