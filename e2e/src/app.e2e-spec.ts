e0f3a2476959dee219e6817112e2c09c323f7672
bednayb <bednayb@gmail.com>
2019-10-25 12:34:53 +0200

    add basic e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,19 +1,131 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
 
+  // TITLES
   const contactPageTitle = 'Contacts';
+  const newContactPageTitle = 'Add New Contact';
+  const notFoundPageTitle = '404 - Page Not Found';
+  const userNotExistAtDetailsPage = 'User not exist';
+
+  // USERDATA
+  const existdUserId = 54325;
+  const existUserName = 'bample5';
+  const notExistdUserId = 23231;
+
+  // New User
+  const newUserName = 'Matilda';
+  const newUserPhone = '+36202034123';
+  const newUserEmail = 'sample@sample.com';
+
+  // CARDS
+  const cardNumber = 8;
+  const firstCard = '';
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
+  // Positiv tests
+
+  // DELETE METHOD
+  it(`should display ${cardNumber - 1} after delete a Contact`, () => {
+    /* navigate contact*/
+    page.navigateTo(`contacts`);
     expect(page.getTitleText()).toEqual(contactPageTitle);
+    // count Card
+    browser.sleep(1000); // should wait because of the animation
+    expect((page.allElement('.card')).count()).toBe(cardNumber);
+    /* click a card */
+    page.clickElement('.card');
+
+    browser.sleep(1000); // should wait because of the animation
+    expect(page.getTitleText()).toContain(`Profile`);
+    page.clickElement('.warning');
+    browser.sleep(1000); // should wait because of the animation
+    expect(page.getTitleText()).toEqual(contactPageTitle);
+    // count Card
+    expect((page.allElement('.card')).count()).toBe(cardNumber - 1);
   });
+
+  // New Method
+  it(`should display ${cardNumber + 1} after add new Contact`, () => {
+    /* navigate contact*/
+    page.navigateTo(`contacts`);
+    expect(page.getTitleText()).toEqual(contactPageTitle);
+    // count Card
+    browser.sleep(1000); // should wait because of the animation
+    expect((page.allElement('.card')).count()).toBe(cardNumber);
+    /* click new contact */
+    page.clickElement('.go__new__contact');
+
+    browser.sleep(1000); // should wait because of the animation
+    expect(page.getTitleText()).toEqual(newContactPageTitle);
+
+    // set input fields
+    page.setInputField('input[name="name"]', `${newUserName}`);
+    page.setInputField('input[name="email"]', `${newUserEmail}`);
+    page.setInputField('input[name="phone"]', `${newUserPhone}`);
+
+    page.clickElement('button');
+    browser.sleep(1000); // should wait because of the animation
+    expect(page.getTitleText()).toEqual(contactPageTitle);
+    // count Card
+    expect((page.allElement('.card')).count()).toBe(cardNumber + 1);
+  });
+
+  // Update Method
+  it(`should display ${cardNumber} after add new Contact`, () => {
+    /* navigate contact*/
+    page.navigateTo(`contacts`);
+    expect(page.getTitleText()).toEqual(contactPageTitle);
+    // count Card
+    browser.sleep(1000); // should wait because of the animation
+    expect((page.allElement('.card')).count()).toBe(cardNumber);
+    /* click new contact */
+    page.clickElement('.card');
+
+    browser.sleep(1000); // should wait because of the animation
+    expect(page.getTitleText()).toContain(`Profile`);
+
+    page.clickElement('#edit');
+
+    // set input fields
+    page.setInputField('input[name="name"]', `${newUserName}`);
+    page.setInputField('input[name="email"]', `${newUserEmail}`);
+    page.setInputField('input[name="phone"]', `${newUserPhone}`);
+
+    page.clickElement('button');
+    browser.sleep(1000); // should wait because of the animation
+    expect(page.getTitleText()).toEqual(contactPageTitle);
+    // count Card
+    expect((page.allElement('.card')).count()).toBe(cardNumber);
+    expect(page.getCardTitleText()).toBe(newUserName);
+
+  });
+
+
+
+
+
+  // Negative tests
+  it(`should display ${userNotExistAtDetailsPage} title at Details page`, () => {
+    page.navigateTo(`contacts/profile/details/${notExistdUserId}`);
+    expect(page.getTitleText()).toEqual(userNotExistAtDetailsPage);
+  });
+
+  it(`should display ${notFoundPageTitle} title at Not found page`, () => {
+    page.navigateTo(`itsnotvalidroot`);
+    expect(page.getTitleText()).toEqual(notFoundPageTitle);
+  });
+
+
+
+
+
+
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,28 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+  navigateTo(url: string) {
+    return browser.get(url) as Promise<any>;
   }
 
   getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+    return element(by.css('h1')).getText() as Promise<string>;
+  }
+
+  clickElement(css: string) {
+    return element(by.css(css)).click();
+  }
+
+  allElement(css: string) {
+    return element.all(by.css(css));
+  }
+
+  getCardTitleText() {
+    return element(by.css('.card h4')).getText() as Promise<string>;
+  }
+
+  async setInputField(css: string, text: string) {
+    await element(by.css(css)).clear();
+    return element(by.css(css)).sendKeys(text);
   }
 }
